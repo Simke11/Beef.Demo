94a8c9dae9631f445377495e3cf7c62e8de8f2f5
Marko Simic <marko.simic@avanade.com>
2020-07-10 11:06:16 +1000

    Initial codegen definition

---
+++ Beef.Demo.CodeGen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Beef.CodeGen;
+using System;
+using System.Threading.Tasks;
+
+namespace Beef.Demo.CodeGen
+{
+    class Program
+    {
+        static Task<int> Main(string[] args)
+        {
+            return CodeGenConsoleWrapper.Create("Beef", "Demo").Supports(entity: true, refData: true).RunAsync(args);
+        }
+    }
+}
