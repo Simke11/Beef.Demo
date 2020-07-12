2a1b48a4866e9d1ce93883de9261e7b187ced507
Marko Simic <marko.simic@avanade.com>
2020-07-13 08:58:16 +1000

    Database project

---
+++ Beef.Demo.Database/Program.cs
@@ -0,0 +1,13 @@
+﻿using Beef.Database.Core;
+using System.Threading.Tasks;
+
+namespace Beef.Demo.Database
+{
+    public class Program
+    {
+        static Task<int> Main(string[] args)
+        {
+            return DatabaseConsoleWrapper.Create("Data Source=.;Initial Catalog=Beef.Demo;Integrated Security=True", "Beef", "Demo").RunAsync(args);
+        }
+    }
+}
